c7872e7151ce5b910e138026d592353af19645f4
O Teerasak Vichadee <iolumin@gmail.com>
2023-12-01 19:19:40 +0700

    add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,141 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, ChipDelete, CircularProgress, CssBaseline, FormControl, IconButton, Input, List, ListItem, Sheet, Tooltip, Typography } from '@mui/joy';
+import { useForm, Controller } from 'react-hook-form';
+import { FC, useState } from 'react';
+
+type Todo = {
+  id: number|string
+  title: string
+}
+
+type TodoItemProps = {
+  text: string
+  onDelete?: () => void
+}
+
+type TodoFormInput = {
+  title: string
+}
+
+type DeleteTodoButtonProps = {
+  onClick?: () => void
+}
+
+const DeleteTodoButton: FC<DeleteTodoButtonProps> = ({ onClick }) => (
+  <Tooltip title="delete">
+    <IconButton onClick={onClick}><ChipDelete /></IconButton>
+  </Tooltip>
+)
+
+const TodoItem: FC<TodoItemProps> = ({ text, onDelete }) => (
+  <ListItem endAction={<DeleteTodoButton onClick={onDelete} />}>
+    {text}
+  </ListItem>
+)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      title: ''
+    }
+  })
+
+  const [error, setError] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, title: 'Dummy' },
+    { id: 2, title: 'Yummy' },
+    { id: 3, title: 'Gummy' },
+    { id: 4, title: 'Tummy' },
+  ])
+
+  function addTodo(data: TodoFormInput): void {
+    const { title } = data 
+
+    if (title.length <= 0) {
+      setError(true)
+      return
+    }
+
+    const todo: Todo = {
+      id: crypto.randomUUID(),
+      title,
+    }
+
+    setLoading(true)
+    setTodos(prev => [...prev, todo])
+
+    setTimeout(() => {
+      setError(false)
+      setLoading(false)
+      reset()
+    }, 1000)
+  }
+
+  function deleteTodo(id: Todo['id']): void {
+    setTodos(prev => prev.filter(item => item.id !== id))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <CssBaseline>
+      <main>
+        <Sheet sx={{
+          width: 300,
+          mx: 'auto',
+          my: 4,
+          py: 3,
+          px: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          borderRadius: 'sm',
+          boxShadow: 'md',
+        }}>
+          <Typography level='h4' component='h1'>Yummy List</Typography>
+          <form onSubmit={handleSubmit(addTodo)}>
+            <FormControl error={error} disabled={loading}>
+              <Controller 
+                name='title'
+                control={control}
+                render={({ field }) => (
+                  <Input 
+                    {...field}
+                    type="text" 
+                    placeholder='Enter yummy food' 
+                    endDecorator={
+                      <>
+                        {!loading && <Button variant='soft' type='submit'>Add</Button>}
+                        {loading && <IconButton disabled><CircularProgress /></IconButton>}
+                      </>
+                    }
+                    />
+                )}
+                />
+            </FormControl>
+          </form>
+          <Sheet 
+            variant='outlined' 
+            sx={{ borderRadius: 'sm', overflow: 'auto', maxHeight: 300 }}
+          >
+            {todos.length <= 0 && (
+              <Sheet sx={{ textAlign: 'center', p: '1em', color: 'GrayText' }}>
+                Empty tummy
+              </Sheet>
+              )}
+            {todos.length > 0 && (
+              <List>
+                {todos.map(todo => (
+                  <TodoItem 
+                    key={todo.id}
+                    text={todo.title}
+                    onDelete={() => deleteTodo(todo.id)}
+                  />
+                ))}
+              </List>
+              )}
+          </Sheet>
+        </Sheet>
+      </main>
+    </CssBaseline>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <CssVarsProvider>
+        <App />
+      </CssVarsProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
 )
