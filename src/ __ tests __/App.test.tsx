814f2adfe82c5a032d4b0009bf3687f309f45e0d
natchanon pornprasatpol <natchanon.por@cpf.co.th>
2023-12-07 14:36:01 +0700

    add unit test

---
+++ src/ __ tests __/App.test.tsx
@@ -0,0 +1,28 @@
+// import { render, screen } from "@testing-library/react";
+// import App from "../App";
+
+// describe("App", () => {
+//   test("renders Yummy List to be in DOM", () => {
+//     render(<App />);
+// const linkElement = screen.getByText(/Yummy List/i);
+// expect(linkElement).toBeInTheDocument();
+//   });
+// });
+
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+test("Renders the main page", () => {
+  render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+  // expect(true).toBeTruthy();
+  const linkElement = screen.getByText(/Yummy List/i);
+  expect(linkElement).toBeInTheDocument();
+});
